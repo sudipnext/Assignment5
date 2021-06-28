ad4cdc4e87181110fd50feb4410cf828fd49aff4
Sudip Parajuli <coc42060@gmail.com>
2021-06-28 07:30:42 +0545

    question 3 solved

---
+++ Assignment7/assignment_7_complete.c
@@ -0,0 +1,70 @@
+// 1.WAP to read 10 elements in an array and calculate the sum and average of all elements.
+// #include<stdio.h>
+// int main(){
+//     int arr[9];
+//     int sum =0;
+//     int average;
+//     for (int i =0; i< 10; i++){
+//         printf("Enter your %d number:-", i+1);
+//         scanf("%d", &arr[i]);
+//         sum += arr[i];
+//         average = sum/10;
+//     }
+//     printf("The sum of array is %d and average is %d  \n", sum, average);
+        // return 0;
+// }
+
+// 2.WAP to read 10 elements in an array and count the occurrence of odd and even numbers.
+// #include <stdio.h>
+// int main(){
+//    int a[9],i,even=0,odd=0;
+
+
+//     for(i=0; i<10; i++)
+//     {
+//         printf("Enter %d element in array : ", i+1);
+//         scanf("%d",&a[i]);
+//     }
+ 
+//      for(i=0; i<10; i++)
+//     {
+//           if(a[i]%2==0)
+//           even++;
+//           else
+//           odd++;
+		           
+//     }
+//      printf("even numbers in array: %d",even);
+//      printf("\n odd numbers in array: %d",odd);
+ 
+ 
+//     return 0;
+// }
+// 3.WAP to read 10 elements in an array and copy all the elements to another array.
+// #include <stdio.h>
+// int main(){
+//     int from[10], destiny[10], i;
+//      printf("Enter the numbers :- ");
+//     for (i=0; i< 10; i++){
+//         scanf("%d", &from[i]);   
+//     }
+//     for (i=0; i< 10; i++){
+//         destiny[i] = from[i];
+//     }
+//     printf("\nElement in first array\n");
+//     for (i=0; i< 10; i++)
+//     {
+//         printf("%d\t", from[i]);
+//     }
+//     printf("\nElements in second array\n");
+//     for (i=0; i< 10; i++){
+//         printf("%d\t", destiny[i]);   
+//     }  
+//     return 0;
+// }
+
+// 4.WAP to read 10 elements in an array and copy all the elements in reverse order to an another array.
+#include<stdio.h>
+int main(){
+    
+}
